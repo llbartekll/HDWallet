8840b097c1c7daffbaa4342958ee24beb317ed36
Bartek <bartek.rozwarski@brightinventions.com>
2021-06-07 08:52:01 +0200

    a

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "HDWallet",
             dependencies: ["CryptoSwift",
-                           .product(name: "secp256k1.swift", package: "secp256k1.swift")],
+                           .product(name: "secp256k1", package: "secp256k1.swift")],
             path: "HDWalletKit",
             sources: ["."]),
         .testTarget(
