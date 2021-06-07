bcfd41156967210db8931e2ca3644fe9635c9eaf
Bartek <bartek.rozwarski@brightinventions.com>
2021-06-07 10:09:41 +0200

    x

--- Package.swift
+++ Package.swift
@@ -13,7 +13,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.0.0"),
-        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", from: "0.2.0")
+        .package(
+            name: "secp256k1",
+            url: "https://github.com/GigaBitcoin/secp256k1.swift.git",
+            from: "0.3.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
