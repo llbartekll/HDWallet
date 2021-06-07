e68bd1cd6fe5ab1ad78d4290b0fd699380c76661
Bartek <bartek.rozwarski@brightinventions.com>
2021-06-07 08:39:07 +0200

    savepoint

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HDWallet",
-            dependencies: ["CryptoSwift", "secp256k1.swift"],
+            dependencies: ["CryptoSwift", "secp256k1"],
             path: "HDWalletKit",
             sources: ["."]),
         .testTarget(
